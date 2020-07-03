2029957e8ed844c32cdf0a08bf13bd05cbf46a1b
Awtanabe <panser098@gmail.com>
2020-07-04 02:49:05 +0900

    Gameのコンポーネント追加

---
+++ src/components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css'
+import Board from '../components/Board'
+
+class Game extends React.Component {
+  
+  render() {
+    return(<Board />)
+  }
+}
+
+export default Game;
