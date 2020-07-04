88db6110bc8f91f14ee15ebc5db8185f80abda5a
Awtanabe <panser098@gmail.com>
2020-07-04 10:21:19 +0900

    タイムトラベル機能

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Board from './components/Board';
+import Game from './components/Game';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Board />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
